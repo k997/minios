93cdd36291afc00d1e04b6389b7e4e40d4bc561d
k997 <26889548+k997@users.noreply.github.com>
2022-01-02 19:51:06 +0800

    内存分配互斥访问

--- kernel/memory/page_alloc.c
+++ kernel/memory/page_alloc.c
@@ -2,6 +2,8 @@
 #include "bitmap.h"
 #include "string.h"
 #include "debug.h"
+#include "sync.h"
+
 /*
     页面分配流程
     virtual_page_alloc 分配虚拟内存,返回虚拟内存地址
@@ -17,24 +19,61 @@
 // 页表项索引，虚拟地址中间 10 位
 #define PTE_INDEX(addr) ((addr & 0x003ff000) >> 12)
 
+// 位图也位于低端 1MB 内存之中，低端 1MB 物理内存不在内存管理单元的管理范围之内
+#define MEM_BITMAP_BASE 0xc009a000
+
+/*
+    内核堆起始地址
+    0xc0000000～0xc00fffff 映射到了物理地址 0x00000000～0x000fffff（低端 1MB 的内存）
+    为了让虚拟地址连续，将堆的起始虚拟地址设为 0xc0100000
+    实际上可以不连续
+ */
+#define K_HEAP_START 0xc0100000
+
+// 该地址由 loader 中地址计算而来
+#define TOTAL_MEM_ADDR 0x903
+
+// 区分内核内存池及用户内存池
+typedef enum
+{
+    PF_KERNEL = 1,
+    PF_USER = 2
+} pool_flag;
+
+// 内核虚拟内存池
+pool kernel_virtual_pool;
+// 内核物理内存池，用户物理内存池
+static pool kernel_phy_pool, user_phy_pool;
+// 内存池互斥访问
+static lock kernel_pool_lock, user_pool_lock;
+
 static void *physical_page_alloc(pool_flag pf);
 static void *virtual_page_alloc(pool_flag pf, uint32_t cnt);
 static void map_vaddr2phyaddr(void *_vaddr, void *_phyaddr);
+static void *page_alloc(pool_flag pf, uint32_t cnt);
 static void *pool_page_alloc(pool *current_pool, uint32_t cnt);
 static uint32_t *pde_ptr(uint32_t vaddr);
 static uint32_t *pte_ptr(uint32_t vaddr);
+static void physical_mem_pool_init();
+static void kernel_virtual_mem_pool_init();
 
 void *kernel_page_alloc(uint32_t cnt)
 {
-    return page_alloc(PF_KERNEL, cnt);
+    lock_acquire(&kernel_pool_lock);
+    void* addr = page_alloc(PF_KERNEL, cnt);
+    lock_release(&kernel_pool_lock);
+    return addr;
 }
 
 void *user_page_alloc(uint32_t cnt)
 {
-    return page_alloc(PF_USER, cnt);
+    lock_acquire(&user_pool_lock);
+    void* addr = page_alloc(PF_USER, cnt);
+    lock_release(&user_pool_lock);
+    return addr;
 }
 
-void *page_alloc(pool_flag pf, uint32_t cnt)
+static void *page_alloc(pool_flag pf, uint32_t cnt)
 {
     void *vaddr_start = virtual_page_alloc(pf, cnt);
     if (vaddr_start == NULL)
@@ -49,7 +88,7 @@ void *page_alloc(pool_flag pf, uint32_t cnt)
         if (paddr == NULL)
         {
             // 分配失败应全部释放已分配的内存,此处暂未实现
-            return NULL; 
+            return NULL;
         }
         map_vaddr2phyaddr((void *)vaddr, paddr);
         vaddr += PG_SIZE;
@@ -156,4 +195,64 @@ static uint32_t *pte_ptr(uint32_t vaddr)
                                  PTE_INDEX(vaddr) * 4);
 
     return pte;
+}
+
+// 初始化内核物理内存池、用户物理内存池、内核虚拟地址池
+void mem_pool_init()
+{
+    lock_init(&kernel_pool_lock);
+    lock_init(&user_pool_lock);
+    physical_mem_pool_init();
+    kernel_virtual_mem_pool_init();
+}
+
+// 内核虚拟地址池初始化
+static void kernel_virtual_mem_pool_init()
+{
+    // 内核虚拟内存池的位图长度同内核物理内存池的位图长度相同
+    kernel_virtual_pool.btmp.byte_length = kernel_phy_pool.btmp.byte_length;
+    kernel_virtual_pool.btmp.bits = (void *)(MEM_BITMAP_BASE + kernel_phy_pool.btmp.byte_length +
+                                             user_phy_pool.btmp.byte_length);
+    kernel_virtual_pool.addr_start = K_HEAP_START;
+    kernel_virtual_pool.size = MEM_MAX_SIZE;
+    bitmap_init(&kernel_virtual_pool.btmp);
+}
+
+/*
+    内存管理单元只管理空闲内存
+    固定占用的内存（即页表与低端 1 MB 内存）不在内存管理单元的管理范围之内
+    低端 1 MB 物理地址： 0x00000～0xfffff，内核虚拟地址 0xc0000000～0xc00fffff
+    页表地址物理地址：0x100000～0x101fff，内核虚拟地址 0xc0100000～0xc0101fff 不能映射到该物理地址处
+ */
+// 物理内存池初始化
+static void physical_mem_pool_init()
+{
+    uint32_t total_mem = (*(uint32_t *)(TOTAL_MEM_ADDR));
+
+    // 在loader中属于内核的页表已经全部创建完成
+    // pde 占 1 页， 第 0 和第 768 个 页目录项相同，占 1 页
+    // 第 769～1022 个页目录项共指向 254 个页表
+    // 256 = 1 + 1 + 254
+    uint32_t page_table_size = PG_SIZE * 256; // 页表占用内存
+    // 0x100000 为低端 1MB 内存
+    uint32_t used_mem = page_table_size + 0x100000;
+    uint32_t free_mem = total_mem - used_mem;
+    uint16_t total_free_pages = free_mem / PG_SIZE;
+    // 内核物理内存与用户物理内存各占一半，分别管理
+    uint16_t kernel_free_pages = total_free_pages / 2;
+    uint16_t user_free_pages = total_free_pages - kernel_free_pages;
+
+    // 初始化内核物理内存池
+    kernel_phy_pool.addr_start = used_mem;
+    kernel_phy_pool.size = kernel_free_pages * PG_SIZE;
+    kernel_phy_pool.btmp.byte_length = kernel_free_pages / 8;
+    kernel_phy_pool.btmp.bits = (void *)MEM_BITMAP_BASE;
+    bitmap_init(&kernel_phy_pool.btmp);
+
+    // 初始化用户物理内存池
+    user_phy_pool.addr_start = kernel_phy_pool.addr_start + kernel_phy_pool.size;
+    user_phy_pool.size = user_free_pages * PG_SIZE;
+    user_phy_pool.btmp.byte_length = user_free_pages / 8;
+    user_phy_pool.btmp.bits = (void *)(MEM_BITMAP_BASE + kernel_phy_pool.btmp.byte_length);
+    bitmap_init(&user_phy_pool.btmp);
 }
--- lib/kernel/memory.h
+++ lib/kernel/memory.h
@@ -23,18 +23,12 @@ typedef struct
                          // 若为虚拟内存池，则设置为 MEM_MAX_SIZE
 } pool;
 
-// 区分内核内存池及用户内存池
-typedef enum
-{
-    PF_KERNEL = 1,
-    PF_USER = 2
-} pool_flag;
+
 
 // 外部引用声明
-extern pool kernel_phy_pool, user_phy_pool, kernel_virtual_pool;
-
+extern pool kernel_virtual_pool;
 void mem_init(void);
-void *page_alloc(pool_flag pf, uint32_t cnt);
+void mem_pool_init(void);
 void *kernel_page_alloc(uint32_t cnt);
 void *user_page_alloc(uint32_t cnt);
 #endif
