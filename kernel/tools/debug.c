d9869b72b939087fd23e61825aec7087bda463d4
kong <26889548+k997@users.noreply.github.com>
2022-09-16 21:16:47 +0800

    优化 panic_spin 输出

--- kernel/tools/debug.c
+++ kernel/tools/debug.c
@@ -1,15 +1,17 @@
 #include "debug.h"
-#include "print.h"
+#include "kstdio.h"
 #include "interrupt.h"
 
-
 // 出现错误则整个程序关中断打印错误信息，os 停止运行
-void panic_spin(char* filename, int line , const char* func,const char* condition){
-    interrupt_disable(); 
-    put_str("\n\n\n===============fatal error===============\n");
-    put_str("filename:");put_str((char*)filename);put_str("\n");
-    put_str("line:0x");put_int(line);put_str("\n");
-    put_str("function:");put_str((char*)func);put_str("\n");
-    put_str("condition:");put_str((char*)condition);put_str("\n");
-    while(1);
+void panic_spin(char *filename, int line, const char *func, const char *condition)
+{
+    interrupt_disable();
+    printk("\n\n\n===============fatal error===============\n"
+           "filename: %s\n"
+           "line: %d\n"
+           "function: %s\n"
+           "condition: %s\n",
+           filename, line, func, condition);
+    while (1)
+        ;
 }
